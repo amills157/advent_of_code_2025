21fe0e4f5dca87e428a06126c18e6c9ed1ea963e
amills157 <alanmills88@gmail.com>
2025-12-01 22:39:10 +0000

    day 1 part 2

--- Day_01/day_one.cs
+++ Day_01/day_one.cs
@@ -5,24 +5,27 @@ using System.Collections.Generic;
 namespace advent_day_1 {
    class puzzle_1 {
 
-      public static  void partOne(ref List<int> intList, ref List<string> directions){
+      public static  void partOne(ref List<int> intList, ref List<string> directions)
+      {
          // Needs to be an array not a list for .Length
          int[] intArray = intList.ToArray();
 
          int starting_number = 50;
 
          int part_one = 0;
-
+         
          for (int i = 0; i < intArray.Length; i++)
          {  
 
             if (directions[i] == "L")
             {  
                starting_number = starting_number -= intArray[i];
+
             }
             else
             {
                starting_number += intArray[i];
+               
             }
             
             starting_number = starting_number % 100;
@@ -35,7 +38,42 @@ namespace advent_day_1 {
          }
 
          Console.WriteLine("part one answer is: {0}", part_one);
+         
+         //Console.WriteLine("part two answer is: {0}", part_one + part_two);
       }
+        
+         // I am sure there is a smart way to do this - But clearly it has elduded me
+         public static void partTwo(ref List<int> intList, ref List<string> directions)
+         {
+            int pos = 50; 
+            int part_two = 0; 
+            
+            int[] intArray = intList.ToArray();
+            
+            for (int i = 0; i < intArray.Length; i++)
+            {
+               // So instead we'll just count all the damn positions
+               for (int j = 0; j < intArray[i]; j++)
+               {
+                  if (directions[i] == "L")
+                  {
+                     pos = (pos - 1 + 100) % 100;
+                  }
+                  else
+                  {
+                     pos = (pos + 1) % 100; 
+                  }
+      
+                  if (pos == 0)
+                  {
+                     part_two +=1;
+                  }
+                  
+               }
+            }
+        
+            Console.WriteLine("part two answer is: {0}", part_two);
+         }
 
       static void Main(string[] args) 
       {
@@ -54,6 +92,8 @@ namespace advent_day_1 {
          }
 
          partOne(ref intList, ref directions);
+         
+         partTwo(ref intList, ref directions);
 
       }
    }
